f5d727cf1305fe6e9fe1f1f01ac9016012fa1a61
EJCalderon <emiliojaviercalderon@gmail.com>
2022-02-27 22:30:17 -0600

    added databases

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS employees;
+DROP DATABASE IF EXISTS employeeDatabases;
 CREATE DATABASE employees;
 USE employees;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const express = require('express');
-const consoleTable = require('./console.table');
+const consoleTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
